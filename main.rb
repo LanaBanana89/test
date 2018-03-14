37b33bfd9a0e73c4ea433ea41486bf3ee6fbdfe9
LanaBanana89 <32016884+LanaBanana89@users.noreply.github.com>
2018-03-14 14:50:11 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -7,9 +7,9 @@ if Gem.win_platform? && ARGV[0]
   name = name.encode("UTF-8")
 end
 
+test = Test.new
 puts "Тест на уровень общительности. Вам необходимо ответить на #{test.questions.count} впоросов.\n\n"
 
-test = Test.new
 test.scoring_points
 
 result = ResultPrinter.new
